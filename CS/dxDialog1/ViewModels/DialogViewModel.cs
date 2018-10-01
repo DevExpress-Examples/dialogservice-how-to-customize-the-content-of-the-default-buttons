d866c6d3b81f1fee09b7dc88ee3376e36ff344c1
Andrey Marten <andrey.kunitsyn@devexpress.com>
2018-10-01 21:47:33 +0300

    add example content

---
+++ CS/dxDialog1/ViewModels/DialogViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.UI;
+
+namespace dxDialog1.ViewModels {
+    public class DialogViewModel : ViewModelBase {
+
+    }
+}
---
+++ CS/dxDialog1/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace dxDialog1.ViewModels {
+    public class MainViewModel : ViewModelBase {
+        protected IDialogService DialogService { get { return this.GetService<IDialogService>(); } }
+
+        DialogViewModel dialogViewModel = null;
+
+        public MainViewModel() {
+            ShowDialogCommand = new DelegateCommand(ShowDialog);
+        }
+
+        public DelegateCommand ShowDialogCommand { get; private set; } 
+
+        public async void ShowDialog() {
+            if(dialogViewModel == null)
+                dialogViewModel = new DialogViewModel();
+
+            var result = await DialogService.ShowDialogAsync(MessageButton.YesNoCancel,
+                "My Dialog Window",
+                dialogViewModel);
+
+        }
+
+    }
+}
